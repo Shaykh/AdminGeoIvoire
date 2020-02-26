fdb775b8938770101f4d9ec0f4922b1cdeb5af1b
Diaby Cheick <diabycheick@live.fr>
2020-02-26 17:00:41 +0100

    Ajout de méthodes

--- AdminGeoIvoire.DataLayerEF/GeoDbContextQueries.cs
+++ AdminGeoIvoire.DataLayerEF/GeoDbContextQueries.cs
@@ -8,8 +8,22 @@ using AdminGeoIvoire.Data.Geo;
 
 namespace AdminGeoIvoire.DataLayerEF
 {
-    public class GeoDbContextQueries
+    public static class GeoDbContextQueries
     {
-        
+        public static async Task<Region> GetRegionAsync(this GeoDbContext dbContext, Region region)
+            => await dbContext.Regions.FirstOrDefaultAsync(item => item.ID == region.ID);
+
+        public static Region GetRegionByLibelle(this GeoDbContext dbContext, string libelle)
+            => dbContext.Regions.FirstOrDefault(item => item.Libelle.ToUpper() == libelle.ToUpper());
+
+        public static async Task<Departement> GetDepartementAsync(this GeoDbContext dbContext, Departement departement)
+            => await dbContext.Departements.FirstOrDefaultAsync(item => item.ID == departement.ID);
+
+        public static async Task<SousPrefecture> GetSousPrefectureAsync(this GeoDbContext dbContext, SousPrefecture sousPrefecture)
+            => await dbContext.SousPrefectures.FirstOrDefaultAsync(item => item.ID == sousPrefecture.ID);
+
+        public static async Task<Village> GetVillagesync(this GeoDbContext dbContext, Village village)
+            => await dbContext.Villages.FirstOrDefaultAsync(item => item.ID == village.ID);
+
     }
 }
