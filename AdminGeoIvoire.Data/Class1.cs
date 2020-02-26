70154036253cb70c67284b2eb4a0c91139c81ff4
Diaby Cheick <diabycheick@live.fr>
2020-02-26 11:04:03 +0100

    Création de l'interface IEntite

--- AdminGeoIvoire.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AdminGeoIvoire.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ AdminGeoIvoire.Data/IEntite.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AdminGeoIvoire.Data
+{
+    public interface IEntite
+    {
+         string  Createur { get; set; }
+
+        DateTime? DateCreation { get; set; }
+
+        string  DernierModificateur { get; set; }
+
+        DateTime? DateDerniereModification { get; set; }
+    }
+}
